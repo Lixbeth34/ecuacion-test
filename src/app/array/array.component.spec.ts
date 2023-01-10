1c6baeb9c53335a14d2a4853e8d64ce7b75f8944
Lixbeth34 <lixbethcaballero02@gmail.com>
2023-01-10 17:43:44 -0600

    add unit-testing #2

--- src/app/array/array.component.spec.ts
+++ src/app/array/array.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import {calculo} from './array.component';
+import {ArrayComponent} from './array.component';
 
-import { ArrayComponent } from './array.component';
+describe('y', () => {
+  it('shoult return y', () => {
+    const resutl = calculo([0.0,1.0,2.0,3.0],2.0,1.0);
+    expect(resutl).toEqual([1,3,5,7])
+  })
+})
 
-describe('ArrayComponent', () => {
-  let component: ArrayComponent;
-  let fixture: ComponentFixture<ArrayComponent>;
+describe('y', () => {
+  it('shoult return y', () => {
+    const resutl = calculo([2.0,1.0,4.0,5.0],3.0,1.0);
+    expect(resutl).toEqual([7,4,13,16])
+  })
+})
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ArrayComponent ]
-    })
-    .compileComponents();
+describe('y', () => {
+  it('shoult return y', () => {
+    const resutl = calculo([7.0,4.0,13.0,16.0],2.0,1.0);
+    expect(resutl).toEqual([15,9,27,33])
+  })
+})
 
-    fixture = TestBed.createComponent(ArrayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+describe('y', () => {
+  it('shoult return y', () => {
+    const resutl = calculo([1.0,2.2,3.0,4.0],3.0,2.0);
+    expect(resutl).toEqual([5,8.600000000000001,11,14])
+  })
+})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+describe('y', () => {
+  it('shoult return y', () => {
+    const resutl = calculo([4.0,3.0,2.0,1.0],1.0,2.0);
+    expect(resutl).toEqual([6,5,4,3])
+  })
+})
+
+//__________________________________________________________________
+
+
--- src/app/array/array.component.ts
+++ src/app/array/array.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./array.component.css']
 })
 export class ArrayComponent {
+  constructor() { }
+arrayx: number[] =[];
+vala=0;
+valb=0;
+show = "";
 
+  btnres(){
+   this.arrayx = calculo(this.arrayx,this.vala,this.valb);
+   this.show = "[" +this.arrayx[0]+","+this.arrayx[1]+","+this.arrayx[2]+","+this.arrayx[3]+ "]";
+  }  
 }
+
+export function calculo(arrx:Array <number>,a :number,b :number){
+  
+  for (let i=0; i< arrx.length;i++){
+    arrx[i] = arrx[i] *a +b;
+  }
+  return arrx;    
+  }
+
