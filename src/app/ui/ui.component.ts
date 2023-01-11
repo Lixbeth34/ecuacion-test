50f4dbd3a256efdabbe325573dbaa72c9f619904
Lixbeth34 <lixbethcaballero02@gmail.com>
2023-01-10 18:08:42 -0600

    add integration-testing #3

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { calculo } from '../array/array.component';
 
 @Component({
   selector: 'app-ui',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./ui.component.css']
 })
 export class UiComponent {
+  arrayx: number[] =[];
+  a=0;
+  b=0;
+  Result: any;
+  vala=0;
+  valb=0;
+  show = "";
 
+  btnres(){
+    this.arrayx = calculo(this.arrayx,this.vala,this.valb);
+    this.show = "[" +this.arrayx[0]+","+this.arrayx[1]+","+this.arrayx[2]+","+this.arrayx[3]+ "]";
+   }  
+
+  calculo(){
+    let myresult = [];
+    let array: number[] = this.arrayx
+    myresult = calculo(array,this.vala,this.valb);
+    this.Result = myresult
+ }
 }
